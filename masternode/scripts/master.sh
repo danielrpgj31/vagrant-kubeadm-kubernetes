b6a17699118eddc8758b4b1b35cc283820e792d9
Daniel Ribeiro <daniel.junior@gmail.com>
2023-12-12 16:06:59 -0300

    Documentacao

--- masternode/scripts/master.sh
+++ masternode/scripts/master.sh
@@ -2,13 +2,7 @@
 #
 # Setup for Control Plane (Master) servers
 
-set -euxo pipefail
-
 NODENAME=$(hostname -s)
-
-sudo kubeadm config images pull
-
-echo "Preflight Check Passed: Downloaded All Required Images"
 
 sudo kubeadm init --apiserver-advertise-address=$CONTROL_IP --apiserver-cert-extra-sans=$CONTROL_IP --pod-network-cidr=$POD_CIDR --service-cidr=$SERVICE_CIDR --node-name "$NODENAME" --ignore-preflight-errors Swap
 
