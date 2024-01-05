fa40446544cc290beeaece616c03dfd1b9b89d9a
Daniel Ribeiro <daniel.junior@gmail.com>
2024-01-05 09:29:00 -0300

    Rename folder

--- masternode/config/lab-jaeger-istio-nodejs/service-customer/build-push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#docker build . -t default-route-openshift-image-registry.apps-crc.testing/default/service-a-nodejs-istio
-#docker push default-route-openshift-image-registry.apps-crc.testing/default/service-a-nodejs-istio
-
-docker build . -t danielribeirojr/service-customer-nodejs-istio
-docker push danielribeirojr/service-customer-nodejs-istio
--- masternode/config/lab-jaeger-istio-nodejs/service-score/build-push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#docker build . -t default-route-openshift-image-registry.apps-crc.testing/default/service-b-nodejs-istio
-#docker push default-route-openshift-image-registry.apps-crc.testing/default/service-b-nodejs-istio
-
-docker build . -t danielribeirojr/service-score-nodejs-istio
-docker push danielribeirojr/service-score-nodejs-istio
